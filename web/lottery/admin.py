6f01c864c2e9542bb95111dbd5238e5762c1aac2
liminglives <liminglives@163.com>
2015-02-15 01:33:49 +0800

    Revert "add order.html ..."
    This reverts commit c28ef462f65dd594c3d57ac9ade9c90470021b50.

--- web/lottery/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from lottery.models import LotteryUser
-
-admin.site.register(LotteryUser)
--- web/lottery/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-
-from lottery import views
-
-urlpatterns = patterns('',
-    url(r'^hello/$', views.hello, name='hello'),
-)
